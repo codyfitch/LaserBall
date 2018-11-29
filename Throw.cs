c5cd71af410dede57da124675d1a12bba6ffa7f1
Cody Fitch <35245717+codyfitch@users.noreply.github.com>
2018-11-29 10:04:27 -0700

    Initial throw mechanic

---
+++ Throw.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour {
+
+    public OVRInput.Controller controller;
+   /* public string buttonName;
+    private GameObject grabbedObject;
+    private bool grabbing;*/
+    public int speed;
+    public GameObject ball;
+
+    void ThrowObject()
+    {
+            ball.GetComponent<Rigidbody>().velocity = OVRInput.GetLocalControllerVelocity(controller) * speed;
+    }
+   /* void DropObject()
+    {
+        grabbing = false;
+
+        if(grabbedObject != null)
+        {
+            grabbedObject.transform.parent = null;
+            grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
+            grabbedObject.GetComponent<Rigidbody>().velocity = OVRInput.GetLocalControllerVelocity(controller) * speed;
+
+            grabbedObject = null;
+        }
+    }
+
+    private void Update()
+    {
+        if (grabbing && Input.GetAxis(buttonName) < 1)
+        {
+            DropObject();
+        }
+    }
+    */
+}
